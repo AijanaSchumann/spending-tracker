2996f24bc397797feeeec34664182ae64b069746
Aijana Schumann <aijana.schumann@gmail.com>
2023-08-18 21:54:52 +1000

    Update Home, add floating action button

--- src/app/views/Home.tsx
+++ src/app/views/Home.tsx
@@ -1,26 +1,26 @@
 import React, { FC } from 'react';
-import { Button, Text, TouchableHighlight } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSelector } from 'react-redux';
 import { selectMonthlySpend, selectMontlyIncome } from '../store/selectors';
-import { RootState } from '../store/store';
 import ExpenseRatio from '../components/home/ExpenseRatio';
 import DataEntryModal from '../components/forms/DataEntryModal';
+import FAB from '../components/general/FAB';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 
-const Home: FC = ({ navigation } : any) =>{
+const Home: FC = () =>{
 
   const monthlySpend = useSelector(selectMonthlySpend);
   const monthlyIncome = useSelector(selectMontlyIncome);
 
-  const [isDataEntryVisible, showDataEntry] = React.useState(false);
+  const [isModalVisible, showModal] = React.useState(false);
   
     return (
-    <SafeAreaView> 
+    <SafeAreaView style={{height:"100%"}}> 
       <ExpenseRatio expenses={monthlySpend} income={monthlyIncome} />
-      <Button title="Add Entry" onPress={()=>{ showDataEntry(true); }} />
-      <DataEntryModal editElement={null} isVisible={isDataEntryVisible} onClose={() => showDataEntry(false)} />
+      <FAB icon={faPlus} color="#189EEC" onPress={()=>{ showModal(true); }}/>
+      <DataEntryModal editElement={null} isVisible={isModalVisible} onClose={() => showModal(false)} />
     </SafeAreaView>)
 }
 
